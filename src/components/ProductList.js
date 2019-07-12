4011b158095df8ae10c7633bf02c4e78986a3ce9
李春霖 <504234675@qq.com>
2019-07-12 20:08:04 +0800

    初始化git提交

---
+++ src/components/ProductList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'dva';
+import { Table, Popconfirm, Button } from 'antd';
+
+@connect(({ products }) => ({
+  products
+}));
+const ProductList = ({ onDelete, products }) => {
+  const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+  }, {
+    title: 'Actions',
+    render: (text, record) => {
+      return (
+        <Popconfirm title="Delete?" onConfirm={() => onDelete(record.id)}>
+          <Button>Delete</Button>
+        </Popconfirm>
+      );
+    },
+  }];
+  return (
+    <Table
+      dataSource={products}
+      columns={columns}
+    />
+  );
+};
+
+ProductList.propTypes = {
+  onDelete: PropTypes.func.isRequired,
+  products: PropTypes.array.isRequired,
+};
+
+export default ProductList;
